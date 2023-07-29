56b53cd8cf8a8fb12d14c155a2ac1091a54719ff
abraker95 <abraker95@gmail.com>
2023-07-29 19:22:26 -0400

    fix config in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,11 +5,11 @@ $(dirname ${0%})/build.sh
 #
 # To be run as root
 systemctl stop forumbot.service
-mv /home/server/prod/osu-forum-feed/config.py /home/server/tmp/config.py
+mv /home/server/prod/osu-forum-feed/config.yaml /home/server/tmp/config.yaml
 mv /home/server/prod/osu-forum-feed/db.json /home/server/tmp/db.json
 rm -rf /home/server/prod/osu-forum-feed
-rsync -av --progress . /home/server/prod/osu-forum-feed --exclude config.py
+rsync -av --progress . /home/server/prod/osu-forum-feed --exclude config.yaml
 chown -R server:server /home/server/prod/osu-forum-feed
-mv /home/server/tmp/config.py /home/server/prod/osu-forum-feed/config.py
+mv /home/server/tmp/config.yaml /home/server/prod/osu-forum-feed/config.yaml
 mv /home/server/tmp/db.json /home/server/prod/osu-forum-feed/db.json
 systemctl start forumbot.service
