e250938096b0911efe595e59cf3cf745f5c5cf49
abraker95 <abraker95@gmail.com>
2024-09-19 18:37:25 -0400

    prob not needed

--- src/core/__init__.py
+++
@@ -1,6 +0,0 @@
-from .ForumMonitor import ForumMonitor
-from .DicordClient import DiscordClient
-from .parser import Post, Topic, User
-from .console_framework import Cmd
-from .BotBase import BotBase
-from .BotException import BotException
