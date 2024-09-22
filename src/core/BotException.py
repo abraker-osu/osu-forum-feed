6f6d4a2aedcc65ce88112c5e1630c9f83af73a87
abraker95 <abraker95@gmail.com>
2024-09-22 16:29:12 -0400

    Dont print traceback portion in BotException if it doesn't exist

--- src/core/BotException.py
+++ src/core/BotException.py
@@ -8,12 +8,17 @@ from .DiscordClient import DiscordClient
 # \TODO: Redo this as a general bot exception to be used for exceptions that we don't want a stacktrace for
 class BotException(Exception):
 
-    def __init__(self, msg: str, show_traceback=True):
+    def __init__(self, msg: str, show_traceback: bool = True):
         Exception.__init__(self, msg)
 
         cwd = f'{os.getcwd()}{os.sep}'
         msg = f"`{msg.replace(cwd, '')}`"
-        trace = f"```py\n{traceback.format_exc().replace(cwd, '')}\n```"
+
+        trace = traceback.format_exc()
+        if trace != 'NoneType: None\n':
+            trace = f"```py\n{traceback.format_exc().replace(cwd, '')}\n```"
+        else:
+            trace = ''
 
         DiscordClient.request('admin/post', {
             'src' : 'forumbot',
