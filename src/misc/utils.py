eecbfb0c17e5ce19011323e02696b79ff8ee919c
abraker95 <abraker95@gmail.com>
2024-09-22 16:28:39 -0400

    Fix crash in tails function when file is smaller than length set to seek back

--- src/misc/utils.py
+++ src/misc/utils.py
@@ -4,20 +4,29 @@ class Utils:
 
     # Thanks https://gist.github.com/amitsaha/5990310
     @staticmethod
-    def tail(lines, fname):
+    def tail(lines: int, fname: str) -> str:
+        """
+        Returns last n lines of a file
+        """
         bufsize = 8192
         fsize   = os.stat(fname).st_size
         iter    = 0
 
-        if fsize < 1: return ''
+        if fsize < 1:
+            return ''
 
-        with open(fname) as f:
+        with open(fname, 'r') as f:
             if bufsize > fsize: bufsize = fsize - 1
 
             data = []
             while True:
                 iter +=1
-                f.seek(fsize-bufsize*iter)
+                loc = fsize - bufsize*iter
+
+                if loc < 0:
+                    return ''.join(data[-lines:])
+
+                f.seek(loc)
                 data.extend(f.readlines())
 
                 print(len(data))
