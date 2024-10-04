0fd0c7cd074fe893c8a9a958d959d8fa02b8cb94
abraker95 <abraker95@gmail.com>
2024-10-03 20:32:02 -0400

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-$(dirname ${0%})/build.sh
+# $(dirname ${0%})/build.sh
 
 # Stuff is copied to a seperate prod location so
 # that edits can be made in this dev location
@@ -16,7 +16,7 @@ mv /home/server/prod/osu-forum-feed/db /home/server/tmp/db
 
 # Nuke the folder and copy over the updated files in repo location -> prod location
 rm -rf /home/server/prod/osu-forum-feed
-rsync -av --progress . /home/server/prod/osu-forum-feed --exclude config.yaml --exclude .git
+rsync -av --progress . /home/server/prod/osu-forum-feed --exclude config.yaml
 chown -R server:server /home/server/prod/osu-forum-feed
 
 # Move config and db files back
@@ -27,10 +27,14 @@ mv /home/server/tmp/db /home/server/prod/osu-forum-feed/db
 # Sensitive files should only be accesible by the user these files are for
 # config: r--r-----
 # db:     rw-rw----
-chown root:server /home/server/prod/sickle-bot/config.yaml
-chmod 440 /home/server/prod/sickle-bot/config.yaml
-chown -R root:server /home/server/prod/sickle-bot/db
-chmod -R 660 /home/server/prod/sickle-bot/db
+chown root:server /home/server/prod/osu-forum-feed/config.yaml
+chmod 440 /home/server/prod/osu-forum-feed/config.yaml
+chown -R root:server /var/lib/forum-bot
+chmod -R 660 /var/lib/forum-bot
+
+# Logs dir
+mkdir -p /var/log/forum-bot
+chown -R root:server /var/log/forum-bot
 
 chmod +x /home/server/prod/osu-forum-feed/scripts/run.sh
 
