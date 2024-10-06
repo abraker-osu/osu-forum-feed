7d8cbb92c9a0c4d247e53bff9eb819ed151a0ca5
abraker95 <abraker95@gmail.com>
2024-10-06 14:52:45 -0400

    condensed exception traceback

--- src/core/BotException.py
+++ src/core/BotException.py
@@ -1,6 +1,5 @@
-import traceback
-import os
-
+import os, sys
+import inspect
 from .DiscordClient import DiscordClient
 
 
@@ -11,16 +10,34 @@ class BotException(Exception):
     def __init__(self, msg: str, show_traceback: bool = True):
         Exception.__init__(self, msg)
 
-        cwd = f'{os.getcwd()}{os.sep}'
-        msg = f"`{msg.replace(cwd, '')}`"
+        trace_text = ''
+        exc_info = sys.exc_info()
 
-        trace = traceback.format_exc()
-        if trace != 'NoneType: None\n':
-            trace = f"```py\n{traceback.format_exc().replace(cwd, '')}\n```"
-        else:
-            trace = ''
+        if exc_info[0] is not None:
+            trace = exc_info[2]
+
+            while True:
+                frame = trace.tb_frame
+                trace_text += f'{frame.f_code.co_filename}:{frame.f_lineno} in {frame.f_code.co_name}\n'
+
+                trace_tmp = trace.tb_next
+                if not trace_tmp:
+                    lines, lineno = inspect.getsourcelines(trace.tb_frame.f_code)
+
+                    line_start = max(0,          trace.tb_frame.f_lineno - lineno - 3)
+                    line_end   = min(len(lines), trace.tb_frame.f_lineno - lineno + 3)
+
+                    trace_text += f''.join(lines[line_start : line_end])
+                    break
+
+                trace = trace_tmp
+
+            trace_text = f'```py\n{trace_text}```'
+
+        msg = f'**{msg}**\n\n' + trace_text
+        msg = msg.replace(f'{os.getcwd()}{os.sep}', '')
 
         DiscordClient.request('admin/post', {
-            'src' : 'forumbot',
-            'contents' : f'**{msg}**\n\n' + trace if show_traceback else msg
+            'src'      : 'forumbot',
+            'contents' : msg
         })
